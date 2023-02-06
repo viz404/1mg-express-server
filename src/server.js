cf6c4341539c8c35cf4e0db347dba87112f5ffa6
Vishal Dinesh <vishal.gxm@gmail.com>
2023-02-07 01:51:06 +0530

    added morgan to print current requests

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const morgan = require("morgan");
 const { connectDatabase } = require("./database/connect");
 const { medicineRouter } = require("./routes/medicineRouter");
 require("dotenv").config();
@@ -10,6 +11,7 @@ app = express();
 // middlewares
 app.use(express.json());
 app.use(cors());
+app.use(morgan("tiny"));
 
 app.get("/", (req, res) => {
   res.send("Server is running");
