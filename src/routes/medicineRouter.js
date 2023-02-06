2d4d7c00a37735f9eceeaad42d1608e2f33eb963
Vishal Dinesh <vishal.gxm@gmail.com>
2023-02-07 01:52:30 +0530

    get filter route added

--- src/routes/medicineRouter.js
+++ src/routes/medicineRouter.js
@@ -2,11 +2,14 @@ const { Router } = require("express");
 const {
   getMedicines,
   getSingleMedicine,
+  getFilters,
+  updateMedicine,
 } = require("../controllers/medicineController");
 
 const medicineRouter = Router();
 
 medicineRouter.get("/", getMedicines);
+medicineRouter.get("/getfilters", getFilters);
 medicineRouter.get("/:id", getSingleMedicine);
 
 module.exports = { medicineRouter };
