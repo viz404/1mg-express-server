c5a0f4c222ec61d52c21f943dd0fcc1503b196cd
Vishal Dinesh <vishal.gxm@gmail.com>
2023-02-10 00:20:26 +0530

    added delete cart , updatequantity and empty cart

--- src/routes/cartRouter.js
+++ src/routes/cartRouter.js
@@ -1,10 +1,19 @@
 const { Router } = require("express");
 const { verifyUser } = require("../controllers/authController");
-const { addtoCart, getCartItems } = require("../controllers/cartController");
+const {
+  addtoCart,
+  getCartItems,
+  updateItemQuantity,
+  deleteCartItem,
+  emptyCart,
+} = require("../controllers/cartController");
 
 const cartRouter = Router();
 
-cartRouter.get("/getcart", verifyUser, getCartItems);
-cartRouter.post("/add", verifyUser, addtoCart);
+cartRouter.get("/getcart", getCartItems);
+cartRouter.delete("/deleteitem", deleteCartItem);
+cartRouter.delete("/emptycart", emptyCart);
+cartRouter.post("/add", addtoCart);
+cartRouter.post("/updatequantity", updateItemQuantity);
 
 module.exports = { cartRouter };
